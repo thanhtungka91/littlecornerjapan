bcb3e488668b196db2b344085eee445dee18d1f6
Vo Thanh Tung <thanhtungka91@gmail.com>
2017-06-24 19:21:16 +0900

    add migrate file

---
+++ db/migrate/20170624091127_create_authentication_methods.spree_social.rb
@@ -0,0 +1,13 @@
+# This migration comes from spree_social (originally 20120123163222)
+class CreateAuthenticationMethods < ActiveRecord::Migration
+  def change
+    create_table :spree_authentication_methods do |t|
+      t.string :environment
+      t.string :provider
+      t.string :api_key
+      t.string :api_secret
+      t.boolean :active
+      t.timestamps null: false
+    end
+  end
+end
