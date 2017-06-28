7011d8530055fd0e625f86cc90b4d262004f5a0e
Vo Thanh Tung <thanhtungka91@gmail.com>
2017-06-29 01:06:15 +0900

    essenstial file for creaktor

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
 
+  mount Ckeditor::Engine => '/ckeditor'
+  # get 'static_pages/home'
+
+  # get 'static_pages/help'
+
+   get 'home', to: 'static_pages#home'
+
   # This line mounts Spree's routes at the root of your application.
   # This means, any requests to URLs such as /products, will go to Spree::ProductsController.
   # If you would like to change where this engine is mounted, simply change the :at option to something different.
