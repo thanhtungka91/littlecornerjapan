7fad5aa8989fd8145a9da5eb8c99f28836bb69bd
Vo Thanh Tung <thanhtungka91@gmail.com>
2017-09-19 02:23:24 +0900

    improve footer

--- app/overrides/homepage_overrides.rb
+++ app/overrides/homepage_overrides.rb
@@ -1,65 +1,231 @@
-Deface::Override.new(virtual_path: 'spree/layouts/spree_application',
-  name: 'add_footer',
-  insert_after: "div.container",
-  text: "
-    <div class='test'>
-    <p>© 2017 Kyanhshoes.com ALL RIGHTS RESERVED </p>
-    <p>Contact information: <a href=mailto:admin@kyanhshoes.com>
-    admin@kyanhshoes.com </a>or Phone Number: 01653541507</p>
-    </a>
-    </div>
-    <style>
-    .test {
-        margin: auto;
-        width: 50%;
-        padding: 10px;
-    }
-    .fb-messengermessageus {
-        position: fixed;
-        heigt: 30px;
-        width: 100%;
-        right: 0;
-        bottom: 0;
-        left: 0;
-        float:center;
-        margin: auto;
-        display: inline-block;
-        font-family: inherit;
-        font-size: 14px;
-        font-weight: bold;
-        color: #fff;
-        text-align: center;
-        background-color: #0084ff;
-        border: 0;
-        cursor: pointer;
-        outline: none;
-    }
-    </style>
+# Deface::Override.new(virtual_path: 'spree/layouts/spree_application',
+#   name: 'add_footer',
+#   insert_after: "div.container",
+#   text: "
+#     <div class='test'>
+#     <p>© 2017 Kyanhshoes.com ALL RIGHTS RESERVED </p>
+#     <p>Contact information: <a href=mailto:admin@kyanhshoes.com>
+#     admin@kyanhshoes.com </a>or Phone Number: 01653541507</p>
+#     </a>
+#     </div>
+#      <link href='//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css' rel='stylesheet'>
+# <!--footer-->
+# <footer class='footer1'>
+# <div class='container'>
 
-    <div class='fb-messengermessageus'
-        messenger_app_id='1737839653123345' 
-        page_id='237931926721099'
-        color='blue'
-        size='large' >
-    </div>    
-    <script>
-        window.fbAsyncInit = function() {
-        FB.init({
-            appId: '1737839653123345',
-            xfbml: true,
-            version: 'v2.6'
-        });
+# <div class='row'><!-- row -->
+            
+#                 <div class='col-lg-3 col-md-3'><!-- widgets column left -->
+#                 <ul class='list-unstyled clear-margins'><!-- widgets -->
+                        
+#                         	<li class='widget-container widget_nav_menu'><!-- widgets list -->
+                    
+#                                 <h1 class='title-widget'>Useful links</h1>
+                                
+#                                 <ul>
+#                                 	<li><a  href='#'><i class='fa fa-angle-double-right'></i> About Us</a></li>
+#                                     <li><a  href='#'><i class='fa fa-angle-double-right'></i> Contact Us</a></li>
+#                                     <li><a  href='#'><i class='fa fa-angle-double-right'></i> Success Stories</a></li>
+#                                     <li><a  href='#'><i class='fa fa-angle-double-right'></i> PG Courses</a></li>
+#                                     <li><a  href='#'><i class='fa fa-angle-double-right'></i> Achiever's Batch</a></li>
+#                                     <li><a  href='#'><i class='fa fa-angle-double-right'></i>  Regular Batch</a></li>
+#                                     <li><a  href='#'><i class='fa fa-angle-double-right'></i>  Test & Discussion</a></li>
+#                                     <li><a  href='#'><i class='fa fa-angle-double-right'></i>  Fast Track T & D</a></li>
+#                                 </ul>
+                    
+# 							</li>
+                            
+#                         </ul>
+                         
+                      
+#                 </div><!-- widgets column left end -->
+                
+                
+                
+#                 <div class='col-lg-3 col-md-3'><!-- widgets column left -->
+            
+#                 <ul class='list-unstyled clear-margins'><!-- widgets -->
+                        
+#                         	<li class='widget-container widget_nav_menu'><!-- widgets list -->
+                    
+#                                 <h1 class='title-widget'>Useful links</h1>
+                                
+#                                 <ul>
+#  									<li><a  href='#'><i class='fa fa-angle-double-right'></i>  Test Series Schedule</a></li>
+#                                     <li><a  href='#'><i class='fa fa-angle-double-right'></i>  Postal Coaching</a></li>
+#                                     <li><a  href='#'><i class='fa fa-angle-double-right'></i>  PG Dr. Bhatia Books</a></li>
+#                                     <li><a  href='#'><i class='fa fa-angle-double-right'></i>  UG Courses</a></li>
+#                                     <li><a  href='#'><i class='fa fa-angle-double-right'></i>  Satellite Education</a></li>
+#                                     <li><a  href='#'><i class='fa fa-angle-double-right'></i>  Study Centres</a></li>
+#                                     <li><a  href='#'><i class='fa fa-angle-double-right'></i>  State P.G. Mocks</a></li>
+#                                     <li><a  href='#' target='_blank'><i class='fa fa-angle-double-right'></i> Results</a></li>
+                                    
+#                                 </ul>
+                    
+# 							</li>
+                            
+#                         </ul>
+                         
+                      
+#                 </div><!-- widgets column left end -->
+                
+                
+                
+#                 <div class='col-lg-3 col-md-3'><!-- widgets column left -->
+            
+#                 <ul class='list-unstyled clear-margins'><!-- widgets -->
+                        
+#                         	<li class='widget-container widget_nav_menu'><!-- widgets list -->
+                    
+#                                 <h1 class='title-widget'>Useful links</h1>
+                                
+#                                 <ul>
 
-        };
 
-        (function(d, s, id){
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) { return; }
-        js = d.createElement(s); js.id = id;
-        js.src = '//connect.facebook.net/en_US/sdk.js';
-        fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
+#                 <li><a href='#'><i class='fa fa-angle-double-right'></i> Enquiry Form</a></li>
+#  				<li><a href='#'><i class='fa fa-angle-double-right'></i> Online Test Series</a></li>
+# 				<li><a href='#'><i class='fa fa-angle-double-right'></i> Grand Tests Series</a></li>
+# 				<li><a href='#'><i class='fa fa-angle-double-right'></i> Subject Wise Test Series</a></li>
+# 				<li><a href='#'><i class='fa fa-angle-double-right'></i> Smart Book</a></li>
+# 				<li><a href='#'><i class='fa fa-angle-double-right'></i> Test Centres</a></li>
+#                 <li><a href='#'><i class='fa fa-angle-double-right'></i>  Admission Form</a></li>
+# 				<li><a href='#'><i class='fa fa-angle-double-right'></i>  Computer Live Test</a></li>
 
-    </script>
-  "
-     )
+#                                 </ul>
+                    
+# 							</li>
+                            
+#                         </ul>
+                         
+                      
+#                 </div><!-- widgets column left end -->
+                
+                
+#                 <div class='col-lg-3 col-md-3'><!-- widgets column center -->
+                
+                   
+                    
+#                         <ul class='list-unstyled clear-margins'><!-- widgets -->
+                        
+#                         	<li class='widget-container widget_recent_news'><!-- widgets list -->
+                    
+#                                 <h1 class='title-widget'>Contact Detail </h1>
+                                
+#                                 <div class='footerp'> 
+                                
+#                                 <h2 class='title-median'>Webenlance Pvt. Ltd.</h2>
+#                                 <p><b>Email id:</b> <a href='mailto:info@webenlance.com'>info@webenlance.com</a></p>
+#                                 <p><b>Helpline Numbers </b>
+
+#     <b style='color:#ffc106;'>(8AM to 10PM):</b>  +91-8130890090, +91-8130190010  </p>
+    
+#     <p><b>Corp Office / Postal Address</b></p>
+#     <p><b>Phone Numbers : </b>7042827160, </p>
+#     <p> 011-27568832, 9868387223</p>
+#                                 </div>
+                                
+#                                 <div class='social-icons'>
+                                
+#                                 	<ul class='nomargin'>
+                                    
+#                 <a href='https://www.facebook.com/bootsnipp'><i class='fa fa-facebook-square fa-3x social-fb' id='social'></i></a>
+# 	            <a href='https://twitter.com/bootsnipp'><i class='fa fa-twitter-square fa-3x social-tw' id='social'></i></a>
+# 	            <a href='https://plus.google.com/+Bootsnipp-page'><i class='fa fa-google-plus-square fa-3x social-gp' id='social'></i></a>
+# 	            <a href='mailto:bootsnipp@gmail.com'><i class='fa fa-envelope-square fa-3x social-em' id='social'></i></a>
+                                    
+#                                     </ul>
+#                                 </div>
+#                     		</li>
+#                           </ul>
+#                        </div>
+#                 </div>
+# </div>
+# </footer>
+# <!--header-->
+
+# <div class='footer-bottom'>
+
+# 	<div class='container'>
+
+# 		<div class='row'>
+
+# 			<div class='col-xs-12 col-sm-6 col-md-6 col-lg-6'>
+
+# 				<div class='copyright'>
+
+# 					© 2015, Webenlance, All rights reserved
+
+# 				</div>
+
+# 			</div>
+
+# 			<div class='col-xs-12 col-sm-6 col-md-6 col-lg-6'>
+
+# 				<div class='design'>
+
+# 					 <a href='#'>Franchisee </a> |  <a target='_blank' href='http://www.webenlance.com'>Web Design & Development by Webenlance</a>
+
+# 				</div>
+
+# 			</div>
+
+# 		</div>
+
+# 	</div>
+
+# </div>
+#     <style>
+#     .test {
+#         margin: auto;
+#         width: 50%;
+#         padding: 10px;
+#     }
+#     .fb-messengermessageus {
+#         position: fixed;
+#         heigt: 30px;
+#         width: 100%;
+#         right: 0;
+#         bottom: 0;
+#         left: 0;
+#         float:center;
+#         margin: auto;
+#         display: inline-block;
+#         font-family: inherit;
+#         font-size: 14px;
+#         font-weight: bold;
+#         color: #fff;
+#         text-align: center;
+#         background-color: #0084ff;
+#         border: 0;
+#         cursor: pointer;
+#         outline: none;
+#     }
+#     </style>
+
+#     <div class='fb-messengermessageus'
+#         messenger_app_id='1737839653123345' 
+#         page_id='237931926721099'
+#         color='blue'
+#         size='large' >
+#     </div>    
+#     <script>
+#         window.fbAsyncInit = function() {
+#         FB.init({
+#             appId: '1737839653123345',
+#             xfbml: true,
+#             version: 'v2.6'
+#         });
+
+#         };
+
+#         (function(d, s, id){
+#         var js, fjs = d.getElementsByTagName(s)[0];
+#         if (d.getElementById(id)) { return; }
+#         js = d.createElement(s); js.id = id;
+#         js.src = '//connect.facebook.net/en_US/sdk.js';
+#         fjs.parentNode.insertBefore(js, fjs);
+#         }(document, 'script', 'facebook-jssdk'));
+
+#     </script>
+#   "
+#      )
