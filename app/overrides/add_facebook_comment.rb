64cd8fb0fa38a7957412f6a597225c801779c25a
Vo Thanh Tung <thanhtungka91@gmail.com>
2017-09-20 23:56:58 +0900

    add facebook comment products

---
+++ app/overrides/add_facebook_comment.rb
@@ -0,0 +1,16 @@
+Deface::Override.new(virtual_path: 'spree/products/show',
+  insert_after: "[data-hook='product_description']",
+  name: 'facebook_plugin',
+  text: "
+  <div id='fb-root'></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = '//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10&appId=132275117399677';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+</script>
+
+<div class='fb-comments' data-href='<%= url_for(:action => 'show', :controller => 'products', :id => @product.slug, :only_path => false) %>' data-width='700' data-numposts='10'></div>
+")
